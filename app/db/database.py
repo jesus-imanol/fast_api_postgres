fae6a2b737b3834c976c8cc2d92637fe81736abc
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-11-07 18:40:06 -0600

    security for database

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:170805somUP1917@lambidb.ca9qg8g6sjuk.us-east-1.rds.amazonaws.com:5432/LAMBIDB"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
